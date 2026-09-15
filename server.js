818b70feab34fc2f16b1eb9a6d90cdbc361c5e66
SHIVAMAL <agreprasad4198@gmail.com>
2026-09-15 14:11:06 +0530

    Change static file serving directory in server.js

--- server.js
+++ server.js
@@ -50,9 +50,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { httpOnly: true, sameSite: "lax", secure: false }
-}));
-app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.static(__dirname));
 function requireAdmin(req, res, next) {
   if (!req.session.admin) return res.status(401).json({ error: "Admin login required." });
   next();
